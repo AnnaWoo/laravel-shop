552a1df4908a7e1ac98181bac7d2647682967fe2
annawoo <chinaanna@outlook.com>
2019-03-05 13:43:30 +0800

    1、省市区联动组件； 2、模板页面； 3、提交表单； 4、中文语言包； 5、添加收货地址入口

--- app/Http/Controllers/UserAddressesController.php
+++ app/Http/Controllers/UserAddressesController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\UserAddressRequest;
+use App\Models\UserAddress;
 use Illuminate\Http\Request;
 
 class UserAddressesController extends Controller
@@ -11,5 +13,28 @@ class UserAddressesController extends Controller
         return view('user_addresses.index', [
             'addresses' => $request->user()->addresses,
         ]);
+    }
+
+    // 新增收货地址页面
+    public function create()
+    {
+        return view('user_addresses.create_and_edit', ['address' => new UserAddress()]);
+    }
+
+    public function store(UserAddressRequest $request)
+    {
+        // $request->only() 通过白名单的方式从用户提交的数据里获取我们所需要的数据
+        $request->user()->addresses()->create($request->only([
+            'province',
+            'city',
+            'district',
+            'address',
+            'zip',
+            'contact_name',
+            'contact_phone',
+        ]));
+
+        // 跳转回我们的地址列表页面
+        return redirect()->route('user_addresses.index');
     }
 }
